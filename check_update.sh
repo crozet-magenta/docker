8b18e375f7941d2236dc276cbc9ee55b443be89d
Cassian Assael <cassian_as@yahoo.fr>
2021-12-10 11:54:27 +0100

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -7,7 +7,7 @@ if [ "$lastTag" != "$currentTag" ]
 then
   echo "Current tag $currentTag is different than last built tag $lastTag => continue build"
   echo "$currentTag" > tag
-  echo "TAG=$currentTag"
+  echo "TAG=$currentTag" >> $GITHUB_ENV
   echo "EXIT=false" >> $GITHUB_ENV
 else
   echo "Current tag $currentTag is the same as last built tag $lastTag => exit build"
